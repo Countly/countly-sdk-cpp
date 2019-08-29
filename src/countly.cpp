5ba48e30b89684dbe18bdb7695ad9b5525d8c98c
Ata Kuyumcu <ata.kuyumcu@yandex.ru>
2019-08-29 12:00:42 +0300

    added options to cmake file

--- src/countly.cpp
+++ src/countly.cpp
@@ -6,21 +6,3 @@ Countly& Countly::getInstance() {
 	static Countly instance;
 	return instance;
 }
-
-std::string Countly::detectOSName() {
-#if defined(TARGET_OS_IPHONE)
-	return "iOS";
-#elif defined(TARGET_OS_MAC)
-	return "macOS";
-#elif defined(_WIN32) || defined(_WIN64) || defined(WIN32)
-	return "Windows";
-#elif defined(__linux__)
-	return "Linux";
-#elif defined(__OpenBSD__)
-	return "Open BSD";
-#elif defined(__unix__)
-	return "Unix";
-#else
-	return "Unknown";
-#endif
-}
