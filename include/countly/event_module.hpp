942fca3ec6c17c52fdcd5640f4a8028a5a902891
Zahid Zafar <zahidzafar51@gmail.com>
2022-08-18 19:25:37 +0500

    event module

---
+++ include/countly/event_module.hpp
@@ -0,0 +1,26 @@
+#ifndef EVENT_MODULE_HPP_
+#define EVENT_MODULE_HPP_
+#include <map>
+#include <memory>
+#include <string>
+
+#include "countly/constants.hpp"
+#include "countly/logger_module.hpp"
+#include "countly/event.hpp"
+
+namespace cly {
+class EventModule {
+
+public:
+  ~EventModule();
+  EventModule(cly::CountlyDelegates *cly, std::shared_ptr<cly::LoggerModule> logger);
+
+  void RecordEvent(const Event &event);
+  void RecordEvent(const std::string &key, const int count = 1, const std::map<std::string, std::string> &segmentation = {}, const double sum = 0, const double duration = 0);
+
+private:
+  class EventModuleImpl;
+  std::unique_ptr<EventModuleImpl> impl;
+};
+} // namespace cly
+#endif
---
+++ src/event_module.cpp
@@ -0,0 +1,36 @@
+#include "countly/event_module.hpp"
+
+#include <chrono>
+
+namespace cly {
+class EventModule::EventModuleImpl {
+
+private:
+  cly::CountlyDelegates *_cly;
+
+public:
+  std::shared_ptr<cly::LoggerModule> _logger;
+  EventModuleImpl(cly::CountlyDelegates *cly, std::shared_ptr<cly::LoggerModule> logger) : _cly(cly), _logger(logger) {}
+
+  ~EventModuleImpl() { _logger.reset(); }
+};
+
+EventModule::EventModule(cly::CountlyDelegates *cly, std::shared_ptr<cly::LoggerModule> logger) {
+  impl.reset(new EventModuleImpl(cly, logger));
+
+  impl->_logger->log(cly::LogLevel::DEBUG, cly::utils::format_string("[ViewsModule] Initialized"));
+}
+
+EventModule::~EventModule() { impl.reset(); }
+
+void EventModule::RecordEvent(const Event &event) {}
+
+void EventModule::RecordEvent(const std::string &key, const int count = 1, const std::map<std::string, std::string> &segmentation = {}, const double sum = 0, const double duration = 0) {
+  Event event(key, count, sum, duration);
+
+  for (auto key_value : segmentation) {
+    event.addSegmentation(key_value.first, key_value.second);
+  }
+}
+
+} // namespace cly
