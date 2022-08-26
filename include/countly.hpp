9300bbfef217a7492c6f0c3e25928d48eecf09e3
Zahid Zafar <zahidzafar51@gmail.com>
2022-08-26 12:13:36 +0500

    Revert "config model as shared ptr"
    This reverts commit dab3b06074c11d50cc34e564c17e4b293a5bf258.

--- include/countly.hpp
+++ include/countly.hpp
@@ -26,7 +26,6 @@
 #include "countly/logger_module.hpp"
 #include "countly/views_module.hpp"
 #include "countly/countly_configuration.hpp"
-#include <countly/request_module.hpp>
 
 namespace cly {
 class Countly : public cly::CountlyDelegates {
@@ -223,6 +222,7 @@ private:
   void updateLoop();
   void (*logger_function)(LogLevel level, const std::string &message) = nullptr;
 
+  int port = 0;
   bool use_https = false;
 
   bool began_session = false;
@@ -233,12 +233,10 @@ private:
 
   nlohmann::json session_params;
 
+  CountlyConfiguration configiration;
   std::unique_ptr<std::thread> thread;
-  std::unique_ptr<ViewsModule> views_module;
-  std::shared_ptr<LoggerModule> logger;
-  std::shared_ptr<RequestModule> request_module;
-  std::shared_ptr<RequestBuilder> request_builder;
-  std::shared_ptr<CountlyConfiguration> configiration;
+  std::unique_ptr<cly::ViewsModule> views_module;
+  std::shared_ptr<cly::LoggerModule> logger;
 
   std::mutex mutex;
   bool stop_thread = false;
