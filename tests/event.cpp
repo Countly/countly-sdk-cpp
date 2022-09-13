6a41986e4a7817717a69aed9afd41221fd8cc647
Zahid Zafar <zahidzafar51@gmail.com>
2022-09-13 15:56:19 +0500

    [Sdk 891] Timestamp issue fixed (#83)
    * namespace added on countly classes
    
    * event timestamp added
    reqeust timestamp updated to milliseconds
    
    * Events and Request unit tests updated
    Timestamp check added
    
    * event common params assert moved into a function.
    
    * Update event.cpp
    
    * Update main.cpp
    
    * Update main.cpp
    
    Co-authored-by: ArtursKadikis <kadikis.arturs@gmail.com>

--- tests/event.cpp
+++ tests/event.cpp
@@ -1,16 +1,30 @@
-#include "countly.hpp"
+#include "countly/event.hpp"
 #include "doctest.h"
+#include <iostream>
+#include <string>
+using namespace cly;
+
+void valideEventParams(nlohmann::json eventJson, std::string key, int count) {
+  CHECK(eventJson["key"].get<std::string>() == key);
+  CHECK(eventJson["count"].get<int>() == count);
+  CHECK(std::to_string(eventJson["timestamp"].get<long long>()).size() == 13);
+}
 
 TEST_CASE("events are serialized correctly") {
   SUBCASE("without segmentation") {
     SUBCASE("without sum") {
       cly::Event event("win", 1);
-      CHECK(event.serialize() == "{\"count\":1,\"key\":\"win\"}");
+
+      nlohmann::json e = nlohmann::json::parse(event.serialize());
+      valideEventParams(e, "win", 1);
     }
 
     SUBCASE("with sum") {
       cly::Event event("buy", 2, 9.99);
-      CHECK(event.serialize() == "{\"count\":2,\"key\":\"buy\",\"sum\":9.99}");
+
+      nlohmann::json e = nlohmann::json::parse(event.serialize());
+      CHECK(e["sum"].get<double>() == 9.99);
+      valideEventParams(e, "buy", 2);
     }
   }
 
@@ -18,51 +32,69 @@ TEST_CASE("events are serialized correctly") {
     SUBCASE("with signed integer") {
       cly::Event event("lose", 3);
       event.addSegmentation("points", -144);
-      CHECK(event.serialize() == "{\"count\":3,\"key\":\"lose\",\"segmentation\":{\"points\":-144}}");
+
+      nlohmann::json e = nlohmann::json::parse(event.serialize());
+      valideEventParams(e, "lose", 3);
+
+      nlohmann::json s = e["segmentation"].get<nlohmann::json>();
+      CHECK(s["points"].get<int>() == -144);
     }
 
     SUBCASE("with unsigned integer") {
       cly::Event event("win", 1);
       event.addSegmentation("points", 232U);
-      CHECK(event.serialize() == "{\"count\":1,\"key\":\"win\",\"segmentation\":{\"points\":232}}");
+
+      nlohmann::json e = nlohmann::json::parse(event.serialize());
+      valideEventParams(e, "win", 1);
+
+      nlohmann::json s = e["segmentation"].get<nlohmann::json>();
+      CHECK(s["points"].get<unsigned int>() == 232U);
     }
 
     SUBCASE("with boolean") {
       cly::Event event("win", 1);
       event.addSegmentation("alive", true);
-      CHECK(event.serialize() == "{\"count\":1,\"key\":\"win\",\"segmentation\":{\"alive\":true}}");
+
+      nlohmann::json e = nlohmann::json::parse(event.serialize());
+      valideEventParams(e, "win", 1);
+
+      nlohmann::json s = e["segmentation"].get<nlohmann::json>();
+      CHECK(s["alive"].get<bool>() == true);
     }
 
     SUBCASE("with string") {
       cly::Event event("message", 1);
       event.addSegmentation("sender", "TheLegend27");
-      CHECK(event.serialize() == "{\"count\":1,\"key\":\"message\",\"segmentation\":{\"sender\":\"TheLegend27\"}}");
+
+      nlohmann::json e = nlohmann::json::parse(event.serialize());
+      valideEventParams(e, "message", 1);
+
+      nlohmann::json s = e["segmentation"].get<nlohmann::json>();
+      CHECK(s["sender"].get<std::string>() == "TheLegend27");
     }
 
     SUBCASE("with multiple values") {
       cly::Event event("buy", 5);
       event.addSegmentation("quantity", 27);
       event.addSegmentation("searchQuery", "cheap cheese");
-      CHECK(event.serialize() == "{\"count\":5,\"key\":\"buy\",\"segmentation\":{\"quantity\":27,\"searchQuery\":\"cheap cheese\"}}");
-    }
 
-    SUBCASE("with changing values") {
-      cly::Event event("lose", 3);
-      event.addSegmentation("points", -144);
-      event.addSegmentation("points", 2000);
-      CHECK(event.serialize() == "{\"count\":3,\"key\":\"lose\",\"segmentation\":{\"points\":2000}}");
-    }
+      nlohmann::json e = nlohmann::json::parse(event.serialize());
+      valideEventParams(e, "buy", 5);
 
-    SUBCASE("with count, sum, duration and segmentation") {
-      cly::Event event("lose", 3, 10, 100);
-      event.addSegmentation("points", 2000);
-      CHECK(event.serialize() == "{\"count\":3,\"dur\":100.0,\"key\":\"lose\",\"segmentation\":{\"points\":2000},\"sum\":10.0}");
+      nlohmann::json s = e["segmentation"].get<nlohmann::json>();
+      CHECK(s["quantity"].get<int>() == 27);
+      CHECK(s["searchQuery"].get<std::string>() == "cheap cheese");
     }
 
     SUBCASE("with multibyte strings") {
       cly::Event event("测试", 1);
       event.addSegmentation("苹果", "美味");
-      CHECK(event.serialize() == "{\"count\":1,\"key\":\"测试\",\"segmentation\":{\"苹果\":\"美味\"}}");
+
+      nlohmann::json e = nlohmann::json::parse(event.serialize());
+      valideEventParams(e, "测试", 1);
+
+      nlohmann::json s = e["segmentation"].get<nlohmann::json>();
+      CHECK(s["苹果"].get<std::string>() == "美味");
     }
   }
 }
