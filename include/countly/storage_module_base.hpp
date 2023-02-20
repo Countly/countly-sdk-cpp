e89a770184b73522ac4455809950b9e292ad0726
zahidzafar51@gmail.com <zahidzafar51@gmail.com>
2023-02-20 12:24:50 +0500

    Storage module methods documentation added.

--- include/countly/storage_module_base.hpp
+++ include/countly/storage_module_base.hpp
@@ -20,8 +20,14 @@ public:
 
   ~DataEntry() {}
 
+  /**
+   * @return id of data entry
+   */
   long long getId() const { return _id; }
 
+  /**
+   * @return content of data entry
+   */
   const std::string &getData() const { return _data; }
 };
 
@@ -40,13 +46,49 @@ public:
     _configuration.reset();
   }
 
+  /**
+   * Initialize the storage module.
+   */
   virtual void init() = 0;
+
+  /**
+   * Returns the count of stored requests.
+   * @return count of stored requests
+   */
   virtual int RQCount() = 0;
+
+  /**
+   * Delete all stored requests.
+   */
   virtual void RQClearAll() = 0;
+
+  /**
+   * Remove front request from the request queue.
+   */
   virtual void RQRemoveFront() = 0;
+
+  /**
+   * Retrieve the element at the front of the queue. It does not deletes the element in the queue.
+   * @return front request of the queue.
+   */
   const virtual std::shared_ptr<DataEntry> RQPeekFront() = 0;
+
+  /**
+   * Retrieve all requests in the request queue without removing them.
+   * @return a vector of requests.
+   */
   virtual std::vector<std::shared_ptr<DataEntry>> RQPeekAll() = 0;
+
+  /**
+   * Remove front request from the request queue.
+   * @param request: a shared pointer to front request
+   */
   virtual void RQRemoveFront(std::shared_ptr<DataEntry> request) = 0;
+
+  /**
+   * Insert element into the request queue at the end.
+   * @param request: content of the request
+   */
   virtual void RQInsertAtEnd(const std::string &request) = 0;
 };
 
