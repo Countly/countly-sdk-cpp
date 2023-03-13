2b453e9498b8f692bbbb5a6b96e0ebb4e738fbec
ArtursK <kadikis.arturs@gmail.com>
2023-03-13 13:28:01 +0200

    2 more scenarios

--- tests/storage_db.cpp
+++ tests/storage_db.cpp
@@ -32,4 +32,28 @@ TEST_CASE("Test Sqlite Storage Module Specific tests") {
     std::shared_ptr<StorageModuleDB> storageModule = std::make_shared<StorageModuleDB>(configuration, logger);
     testStorageModuleWithoutInit(storageModule);
   }
+
+  TEST_CASE("Test SQlite Storage Module against invalid database path") {
+    test_utils::clearSDK();
+    shared_ptr<cly::LoggerModule> logger;
+    logger.reset(new cly::LoggerModule());
+
+    shared_ptr<cly::CountlyConfiguration> configuration = std::make_shared<CountlyConfiguration>("", "");
+    configuration->databasePath = "/";
+
+    std::shared_ptr<StorageModuleDB> storageModule = std::make_shared<StorageModuleDB>(configuration, logger);
+    testStorageModuleWithoutInit(storageModule);
+  }
+
+  TEST_CASE("Test SQlite Storage Module against invalid database path") {
+    test_utils::clearSDK();
+    shared_ptr<cly::LoggerModule> logger;
+    logger.reset(new cly::LoggerModule());
+
+    shared_ptr<cly::CountlyConfiguration> configuration = std::make_shared<CountlyConfiguration>("", "");
+    configuration->databasePath = "Q:/blah";
+
+    std::shared_ptr<StorageModuleDB> storageModule = std::make_shared<StorageModuleDB>(configuration, logger);
+    testStorageModuleWithoutInit(storageModule);
+  }
 #endif
